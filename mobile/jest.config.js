bdaa2c7872056faf7126f6b1ded243df59e1b6e2
therealcoolnerd <101952086+therealcoolnerd@users.noreply.github.com>
2025-06-15 17:12:05 -0500

    test: add cryptography tests and setup

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -5,4 +5,10 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(expo-secure-store|@noble/ed25519)/)'
   ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '../node_modules'],
+  moduleNameMapper: {
+    '^@noble/ed25519$': '<rootDir>/node_modules/@noble/ed25519',
+    '^bs58$': '<rootDir>/node_modules/bs58',
+  },
 };
---
+++ mobile/jest.setup.js
@@ -0,0 +1,16 @@
+const { Buffer } = require('buffer');
+global.Buffer = Buffer;
+
+const path = require('path');
+const Module = require('module');
+const extraPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(__dirname, '../node_modules'),
+];
+process.env.NODE_PATH = extraPaths.join(path.delimiter);
+Module.Module._initPaths();
+extraPaths.forEach((p) => {
+  if (!Module.Module.globalPaths.includes(p)) {
+    Module.Module.globalPaths.push(p);
+  }
+});
