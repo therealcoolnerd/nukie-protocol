a25580502e7fc6ed9a748dc86cec1646c9e0ead3
therealcoolnerd <101952086+therealcoolnerd@users.noreply.github.com>
2025-06-15 21:37:13 -0500

    refactor: centralize cryptography utils

--- mobile/utils/identity/cryptography.js
+++ mobile/utils/identity/cryptography.js
@@ -1,22 +1,2 @@
-import * as ed from "@noble/ed25519";
-import bs58 from "bs58";
+export * from '../../src/utils/identity/cryptography';
 
-const MULTICODEC_ED25519_PREFIX = new Uint8Array([0xed, 0x01]);
-
-export async function generateDIDKey() {
-  const privateKey = ed.utils.randomPrivateKey();
-  const publicKey = await ed.getPublicKey(privateKey);
-  const multicodec = new Uint8Array(
-    MULTICODEC_ED25519_PREFIX.length + publicKey.length,
-  );
-  multicodec.set(MULTICODEC_ED25519_PREFIX, 0);
-  multicodec.set(publicKey, MULTICODEC_ED25519_PREFIX.length);
-  const did = "did:key:z" + bs58.encode(multicodec);
-  return {
-    did,
-    publicKey: Buffer.from(publicKey).toString("hex"),
-    privateKey: Buffer.from(privateKey).toString("hex"),
-  };
-}
-
-export default generateDIDKey;
