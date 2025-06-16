794a712c770497f2971d57ef591f071042e00aea
therealcoolnerd <101952086+therealcoolnerd@users.noreply.github.com>
2025-06-15 21:13:58 -0500

    test: validate src cryptography helper

---
+++ mobile/__tests__/cryptographySrc.test.js
@@ -0,0 +1,20 @@
+import { generateDIDKey } from '../../src/utils/identity/cryptography';
+import * as ed from '@noble/ed25519';
+
+beforeAll(() => {
+  const crypto = require('crypto');
+  ed.etc.sha512Sync = (...msgs) => {
+    const hash = crypto.createHash('sha512');
+    for (const m of msgs) hash.update(Buffer.from(m));
+    return new Uint8Array(hash.digest());
+  };
+});
+
+describe('generateDIDKey - src version', () => {
+  test('returns valid did:key and hex keys', async () => {
+    const { did, privateKey, publicKey } = await generateDIDKey();
+    expect(did).toMatch(/^did:key:z[1-9A-HJ-NP-Za-km-z]+$/);
+    expect(privateKey).toMatch(/^[0-9a-f]{64}$/);
+    expect(publicKey).toMatch(/^[0-9a-f]{64}$/);
+  });
+});
